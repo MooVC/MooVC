e160963a3ec3a8e7491dccb4a8d12eb2bd4dff00
MooVC <vmartins.paul@gmail.com>
2020-01-26 19:53:50 +0000

    Added a Generic Version of GetValue for SerializationInfo Corrected the name of the "Internal" variant of GetValue

--- src/MooVC/Serialization/SerializationInfoExtensions.GetInternalValue.cs
+++ src/MooVC/Serialization/SerializationInfoExtensions.GetInternalValue.cs
@@ -5,9 +5,14 @@
 
     public static partial class SerializationInfoExtensions
     {
-        public static object GetValue(this SerializationInfo info, string name, Type type)
+        public static object GetInternalValue(this SerializationInfo info, string name, Type type)
         {
             return info.GetValue(FormatName(name), type);
+        }
+
+        public static T GetInternalValue<T>(this SerializationInfo info, string name)
+        {
+            return (T)info.GetValue(FormatName(name), typeof(T));
         }
     }
 }
---
+++ src/MooVC/Serialization/SerializationInfoExtensions.GetValue.cs
@@ -0,0 +1,12 @@
+﻿namespace MooVC.Serialization
+{
+    using System.Runtime.Serialization;
+
+    public static partial class SerializationInfoExtensions
+    {
+        public static T GetValue<T>(this SerializationInfo info, string name)
+        {
+            return (T)info.GetValue(name, typeof(T));
+        }
+    }
+}
