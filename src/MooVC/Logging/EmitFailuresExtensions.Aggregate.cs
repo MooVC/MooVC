72b3b5b6b2c7e65e80689bbbc3aea6d7ee679eb3
MooVC <vmartins.paul@gmail.com>
2020-02-11 15:41:12 +0000

    Added an extension that will throw an AggregateException if one or more IEmit implementions emit a Failure/Warning

---
+++ src/MooVC/Logging/EmitFailuresExtensions.Aggregate.cs
@@ -0,0 +1,32 @@
+namespace ControlSoft
+{
+    using System;
+    using System.Collections.Generic;
+    using MooVC.Logging;
+    using static EmitWarningsExtensions;
+
+    public static partial class EmitFailuresExtensions
+    {
+        public static void Aggregate<T>(this T source, Action<T> action, string? message = default)
+            where T : IEmitFailures
+        {
+            new[] { source }.Aggregate(action, message: message);
+        }
+
+        public static void Aggregate<T>(this IEnumerable<T> sources, Action<T> action, string? message = default)
+            where T : IEmitFailures
+        {
+            PerformAggregation(
+                (source, handler) =>
+                {
+                    source.FailureEmitted += handler;
+
+                    action(source);
+
+                    source.FailureEmitted -= handler;
+                },
+                message,
+                sources);
+        }
+    }
+}
