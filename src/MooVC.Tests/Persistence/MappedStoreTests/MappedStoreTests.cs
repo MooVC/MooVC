125de76059933fd8f4548aad1263ed032c4d3f5c
Paul Martins <pmartins@controlsoft.ie>
2020-05-19 11:11:41 +0100

    Added the concept of a mapped store, to allow one key to be mapped to another via the IStore interface

---
+++ src/MooVC.Tests/Persistence/MappedStoreTests/MappedStoreTests.cs
@@ -0,0 +1,22 @@
+﻿namespace MooVC.Persistence.MappedStoreTests
+{
+    using System;
+    using MooVC.Persistence;
+    using Moq;
+
+    public abstract class MappedStoreTests
+    {
+        protected MappedStoreTests()
+        {
+            InnerMapping = key => key.ToString();
+            OutterMapping = (item, key) => new Guid(key);
+            Store = new Mock<IStore<object, string>>();
+        }
+
+        protected Func<Guid, string> InnerMapping { get; }
+
+        protected Func<object, string, Guid> OutterMapping { get; }
+
+        protected Mock<IStore<object, string>> Store { get; }
+    }
+}
