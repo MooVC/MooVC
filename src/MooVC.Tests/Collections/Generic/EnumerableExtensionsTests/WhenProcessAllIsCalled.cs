c67009fb4929e61cb6f64a23d1df3afdc6679e39
Paul Martins <pmartins@controlsoft.ie>
2020-07-12 18:28:14 +0100

    Added new Process and ProcessAll variants of Enumerable extensions to support aggregation of results for enumeration processing

---
+++ src/MooVC.Tests/Collections/Generic/EnumerableExtensionsTests/WhenProcessAllIsCalled.cs
@@ -0,0 +1,53 @@
+﻿namespace MooVC.Collections.Generic.EnumerableExtensionsTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public sealed class WhenProcessAllIsCalled
+    {
+        [Fact]
+        public void GivenASourceThenResultsForThatSourceAreReturned()
+        {
+            IEnumerable<int> source = new[] { 1, 2, 3 };
+            IEnumerable<int> expected = new[] { 1, 4, 9 };
+            IEnumerable<int> results = source.ProcessAll(value => value * value);
+
+            Assert.NotNull(results);
+            Assert.Contains(results, element => expected.Contains(element));
+            Assert.Equal(expected.Count(), results.Count());
+        }
+
+        [Fact]
+        public void GivenASourceAndAnEnumerableResultTransformThenResultsForThatSourceAreReturned()
+        {
+            IEnumerable<int> source = new[] { 1, 2, 3 };
+            IEnumerable<int> expected = new[] { 1, 4, 9 };
+            IEnumerable<int> results = source.ProcessAll(value => new[] { value * value }.AsEnumerable());
+
+            Assert.NotNull(results);
+            Assert.Contains(results, element => expected.Contains(element));
+            Assert.Equal(expected.Count(), results.Count());
+        }
+
+        [Fact]
+        public void GivenANullSourceThenAnEmptySetOfResultsIsReturned()
+        {
+            IEnumerable<int> source = null;
+            IEnumerable<int> results = source.ProcessAll(value => value);
+
+            Assert.NotNull(results);
+            Assert.Empty(results);
+        }
+
+        [Fact]
+        public void GivenANullSourceAndAnEnumerableResultTransformThenAnEmptySetOfResultsIsReturned()
+        {
+            IEnumerable<int> source = null;
+            IEnumerable<int> results = source.ProcessAll(value => new[] { value }.AsEnumerable());
+
+            Assert.NotNull(results);
+            Assert.Empty(results);
+        }
+    }
+}
---
+++ src/MooVC/Collections/Generic/EnumerableExtensions.ProcessAll.cs
@@ -0,0 +1,38 @@
+﻿namespace MooVC.Collections.Generic
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class EnumerableExtensions
+    {
+        public static IEnumerable<TResult> ProcessAll<TResult, TSource>(this IEnumerable<TSource>? source, Func<TSource, TResult> transform)
+        {
+            return source.ProcessAll(
+                source =>
+                {
+                    TResult result = transform(source);
+
+                    if (result is { })
+                    {
+                        return new[] { result };
+                    }
+
+                    return Enumerable.Empty<TResult>();
+                });
+        }
+
+        public static IEnumerable<TResult> ProcessAll<TSource, TResult>(this IEnumerable<TSource>? source, Func<TSource, IEnumerable<TResult>> transform)
+        {
+            ConcurrentBag<TResult>? bag = default;
+
+            return source.Process(
+                result => bag!.Add(result),
+                ForAll,
+                () => bag = new ConcurrentBag<TResult>(),
+                () => bag!.ToArray(),
+                transform);
+        }
+    }
+}
