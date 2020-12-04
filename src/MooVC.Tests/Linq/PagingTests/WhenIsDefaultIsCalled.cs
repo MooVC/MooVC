d50f6c62c08b048612cdb1104c3eee0857c10584
Paul Martins <pmartins@controlsoft.ie>
2020-12-04 15:26:53 +0000

    Added a default instance for Paging

---
+++ src/MooVC.Tests/Linq/PagingTests/WhenIsDefaultIsCalled.cs
@@ -0,0 +1,21 @@
+﻿namespace MooVC.Linq.PagingTests
+{
+    using Xunit;
+
+    public sealed class WhenIsDefaultIsCalled
+    {
+        [Fact]
+        public void GivenThePagingDefaultThenAPositiveResponseIsReturned()
+        {
+            Assert.True(Paging.Default.IsDefault);
+        }
+
+        [Fact]
+        public void GivenAPagingInstanceThenANegativeResponseIsReturned()
+        {
+            var paging = new Paging();
+
+            Assert.False(paging.IsDefault);
+        }
+    }
+}
