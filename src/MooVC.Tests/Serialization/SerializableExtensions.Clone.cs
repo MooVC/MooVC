1735d20640df5eb9f4f05de6d91896be3d7c898e
Paul Martins <pmartins@controlsoft.ie>
2021-01-16 22:15:17 +0000

    Added a new ICloner interface, with a BinaryFormatterCloner provided to support migration from the legacy Clone extension.  This class has been marked as obsolette due to its usage of an obsolette class.

--- src/MooVC.Tests/Serialization/SerializableExtensions.Clone.cs
+++ src/MooVC.Tests/Serialization/SerializableExtensions.Clone.cs
@@ -1,22 +1,15 @@
 namespace MooVC.Serialization
 {
-    using System.IO;
     using System.Runtime.Serialization;
-    using System.Runtime.Serialization.Formatters.Binary;
 
     public static class SerializableExtensions
     {
         public static T Clone<T>(this T original)
             where T : ISerializable
         {
-            var binaryFormatter = new BinaryFormatter();
+            var cloner = new BinaryFormatterCloner();
 
-            using var stream = new MemoryStream();
-
-            binaryFormatter.Serialize(stream, original);
-            _ = stream.Seek(0, SeekOrigin.Begin);
-
-            return (T)binaryFormatter.Deserialize(stream);
+            return cloner.Clone(original);
         }
     }
 }
---
+++ src/MooVC/Serialization/BinaryFormatterCloner.cs
@@ -0,0 +1,25 @@
+﻿namespace MooVC.Serialization
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+
+    [Obsolete("BinaryFormatter serialization is obsolete and should not be used. See https://aka.ms/binaryformatter for more information.", DiagnosticId = "SYSLIB0011", UrlFormat = "https://aka.ms/dotnet-warnings/{0}")]
+    public sealed class BinaryFormatterCloner
+        : ICloner
+    {
+        public T Clone<T>(T original)
+            where T : ISerializable
+        {
+            var binaryFormatter = new BinaryFormatter();
+
+            using var stream = new MemoryStream();
+
+            binaryFormatter.Serialize(stream, original);
+            _ = stream.Seek(0, SeekOrigin.Begin);
+
+            return (T)binaryFormatter.Deserialize(stream);
+        }
+    }
+}
