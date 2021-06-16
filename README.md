dfa614867595d54ecfb4a29dc9fbbd87fd32c81d
Paul Martins <pmartins@controlsoft.ie>
2021-06-16 16:47:59 +0100

    - Changed Serialization.SerializationInfoExtensions.TryGetValue extension to call internal GetValueNoThrow method instead of relying on serialization exception to detect missing values.

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ Added additional support for asynchonrous event handling and cancellation.
 - Changed Diagnostics.EmitDiagnosticsExtensions.Invoke to an async variant named Diagnostics.EmitDiagnosticsExtensions.InvokeAsync (**Breaking Change**).
 - Changed Processing.ProcessorStateChangedEventHandler to an async variant named Processing.ProcessorStateChangedAsyncEventHandler (**Breaking Change**).
 - Changed Threading.Initializer so that the delegate now accepts a cancellation token (**Breaking Change**).
+- Changed Serialization.SerializationInfoExtensions.TryGetValue extension to call internal GetValueNoThrow method instead of relying on serialization exception to detect missing values.
 - Renamed Persistence.IEventStore.ReadAsync for individual reads to GetAsync due to unintended ambiguity (**Breaking Change**).
 
 ## Bug Fixes
--- src/MooVC/Serialization/SerializationInfoExtensions.TryGetValue.cs
+++ src/MooVC/Serialization/SerializationInfoExtensions.TryGetValue.cs
@@ -1,16 +1,28 @@
 ﻿namespace MooVC.Serialization
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
     using System.Runtime.Serialization;
 
     public static partial class SerializationInfoExtensions
     {
+        private const string MethodName = "GetValueNoThrow";
+        private static readonly Lazy<MethodInfo> method;
+
+        static SerializationInfoExtensions()
+        {
+            method = new Lazy<MethodInfo>(CreateMethodInfo);
+        }
+
+        private static MethodInfo Method => method.Value;
+
         [return: NotNullIfNotNull("defaultValue")]
         public static T? TryGetValue<T>(this SerializationInfo info, string name, T? defaultValue = default)
         {
             try
             {
-                object? value = info.GetValue(name, typeof(T));
+                object? value = Method.Invoke(info, new object[] { name, typeof(T) });
 
                 if (value is T result)
                 {
@@ -23,6 +35,15 @@
             {
                 return defaultValue;
             }
+        }
+
+        private static MethodInfo CreateMethodInfo()
+        {
+            Type type = typeof(SerializationInfo);
+
+            return type.GetMethod(
+                MethodName,
+                BindingFlags.Instance | BindingFlags.NonPublic);
         }
     }
 }
