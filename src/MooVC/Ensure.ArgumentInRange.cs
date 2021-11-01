e340950d214410cd32f657130b9b494846010292
Paul Martins <pmartins@controlsoft.ie>
2021-11-01 09:49:52 +0000

    - Added a Ensure.ArgumentInRange to simplify guard conditions where two comparable value types must be within a given inclusive range.

---
+++ src/MooVC/Ensure.ArgumentInRange.cs
@@ -0,0 +1,44 @@
+﻿namespace MooVC
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    public static partial class Ensure
+    {
+        public static T ArgumentInRange<T>(
+            T argument,
+            string argumentName,
+            T? end = default,
+            T? start = default)
+            where T : struct, IComparable<T>
+        {
+            if (start.HasValue && argument.CompareTo(start.Value) < 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName, argument, default);
+            }
+
+            if (end.HasValue && argument.CompareTo(end.Value) > 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName, argument, default);
+            }
+
+            return argument;
+        }
+
+        public static T ArgumentInRange<T>(
+           [NotNull] T? argument,
+           string argumentName,
+           T? end = default,
+           T? start = default)
+           where T : struct, IComparable<T>
+        {
+            T actual = ArgumentNotNull(argument, argumentName);
+
+            return ArgumentInRange(
+                actual,
+                argumentName,
+                end: end,
+                start: start);
+        }
+    }
+}
