008239964a44f460916afe4600ddd09fe9de66b8
MooVC <50200071+MooVC@users.noreply.github.com>
2022-06-30 11:01:29 +0100

    Added additional construction tests for pagedresult

--- src/MooVC.Tests/Linq/PagedResultTests/WhenPagedResultIsConstructed.cs
+++ src/MooVC.Tests/Linq/PagedResultTests/WhenPagedResultIsConstructed.cs
@@ -6,10 +6,55 @@ using Xunit;
 public sealed class WhenPagedResultIsConstructed
 {
     [Theory]
+    [InlineData(1, 10, new[] { 1, 2, 3, 4 })]
+    [InlineData(5, 0, new int[0])]
+    [InlineData(0, 100, new[] { 1 })]
+    public void GivenValuesAndNoTotalThenAllPropertiesAreSet(ushort page, ushort size, int[] values)
+    {
+        var request = new Paging(page: page, size: size);
+
+        var result = new PagedResult<int>(request, values);
+
+        Assert.Equal(request, result.Request);
+        Assert.Equal((ulong)values.LongLength, result.Total);
+        Assert.Equal(values, result.Values);
+    }
+
+    [Theory]
     [InlineData(1, 10, 50, new[] { 1, 2, 3, 4 })]
     [InlineData(5, 0, 20, new int[0])]
     [InlineData(0, 100, 0, new[] { 1 })]
-    public void GivenValuesThenAllPropertiesAreSet(ushort page, ushort size, ulong total, int[] values)
+    public void GivenValuesAndAnIntTotalThenAllPropertiesAreSet(ushort page, ushort size, int total, int[] values)
+    {
+        var request = new Paging(page: page, size: size);
+
+        var result = new PagedResult<int>(request, total, values);
+
+        Assert.Equal(request, result.Request);
+        Assert.Equal((ulong)total, result.Total);
+        Assert.Equal(values, result.Values);
+    }
+
+    [Theory]
+    [InlineData(1, 10, 50, new[] { 1, 2, 3, 4 })]
+    [InlineData(5, 0, 20, new int[0])]
+    [InlineData(0, 100, 0, new[] { 1 })]
+    public void GivenValuesAndALongTotalThenAllPropertiesAreSet(ushort page, ushort size, long total, int[] values)
+    {
+        var request = new Paging(page: page, size: size);
+
+        var result = new PagedResult<int>(request, total, values);
+
+        Assert.Equal(request, result.Request);
+        Assert.Equal((ulong)total, result.Total);
+        Assert.Equal(values, result.Values);
+    }
+
+    [Theory]
+    [InlineData(1, 10, 50, new[] { 1, 2, 3, 4 })]
+    [InlineData(5, 0, 20, new int[0])]
+    [InlineData(0, 100, 0, new[] { 1 })]
+    public void GivenValuesAndAUlongTotalThenAllPropertiesAreSet(ushort page, ushort size, ulong total, int[] values)
     {
         var request = new Paging(page: page, size: size);
 
