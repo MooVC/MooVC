f4e2a46124e19c54fd341b844f799e6e8b63b108
MooVC <50200071+MooVC@users.noreply.github.com>
2022-08-16 16:07:01 +0100

    - Added a Collections.Generic.ObjectExtensions.AsArray extension that will return an array containing the value on which the request was made. - Added a Collections.Generic.ObjectExtensions.AsEnumerable extension that will return an enumerable containing the value on which the request was made.

---
+++ src/MooVC/Collections/Generic/ObjectExtensions.AsArray.cs
@@ -0,0 +1,9 @@
+﻿namespace MooVC.Collections.Generic;
+
+public static partial class ObjectExtensions
+{
+    public static T[] AsArray<T>(this T value)
+    {
+        return new[] { value };
+    }
+}
---
+++ src/MooVC/Collections/Generic/ObjectExtensions.AsEnumerable.cs
@@ -0,0 +1,11 @@
+﻿namespace MooVC.Collections.Generic;
+
+using System.Collections.Generic;
+
+public static partial class ObjectExtensions
+{
+    public static IEnumerable<T> AsEnumerable<T>(this T value)
+    {
+        yield return value;
+    }
+}
