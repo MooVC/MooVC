32a8a1d893a825ba68500fb0fad90e8872239ff1
MooVC <50200071+MooVC@users.noreply.github.com>
2022-08-28 15:33:59 +0100

    Added a default timeout to coordinator

--- src/MooVC/Threading/Coordinator.cs
+++ src/MooVC/Threading/Coordinator.cs
@@ -12,11 +12,19 @@ public sealed class Coordinator<T>
     where T : notnull
 {
     public const string NumberFormat = "X";
+
     private readonly ConcurrentDictionary<string, SemaphoreSlim> contexts = new();
+    private readonly TimeSpan? @default;
+
+    public Coordinator(TimeSpan? @default = default)
+    {
+        this.@default = @default;
+    }
 
     public async Task<ICoordinationContext<T>> ApplyAsync(T context, CancellationToken? cancellationToken = default, TimeSpan? timeout = default)
     {
         context = context ?? throw new ArgumentNullException(nameof(context), CoordinatorApplyAsyncContextRequired);
+        timeout ??= @default;
 
         string key = GetKey(context);
         SemaphoreSlim semaphore = contexts.GetOrAdd(key, _ => new SemaphoreSlim(1, 1));
