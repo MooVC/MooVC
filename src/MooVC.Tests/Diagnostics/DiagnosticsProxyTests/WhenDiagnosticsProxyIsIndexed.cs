4c6d4b5b2d73b8b74bb137d38075041b04efb329
MooVC <50200071+MooVC@users.noreply.github.com>
2022-09-15 17:56:41 +0100

    Added additional tests

---
+++ src/MooVC.Tests/Diagnostics/DiagnosticsProxyTests/WhenDiagnosticsProxyIsIndexed.cs
@@ -0,0 +1,38 @@
+﻿namespace MooVC.Diagnostics.DiagnosticsProxyTests;
+
+using System.Collections.Generic;
+using Xunit;
+
+public sealed class WhenDiagnosticsProxyIsIndexed
+{
+    [Fact]
+    public void GivenAnExistingLevelThenTheLevelIsReturned()
+    {
+        var defaults = new Dictionary<Impact, Level>
+        {
+            { Impact.Recoverable, Level.Critical },
+            { Impact.Unrecoverable, Level.Error },
+            { Impact.Negligible, Level.Warning },
+            { Impact.None, Level.Information },
+        };
+
+        var diagnostics = new DiagnosticsProxy(defaults: defaults);
+
+        foreach (Impact impact in defaults.Keys)
+        {
+            Level actual = diagnostics[impact];
+            Level expected = defaults[impact];
+
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    [Fact]
+    public void GivenANonExistingLevelThenTheLevelIsReturned()
+    {
+        var diagnostics = new DiagnosticsProxy();
+        Level level = diagnostics[(Impact)255];
+
+        Assert.Equal(Level.Error, level);
+    }
+}
