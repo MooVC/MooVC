54b2bccd5af4a35dbfa79418ba070bef862f405c
Paul Martins <vmartins.paul@gmail.com>
2025-10-25 20:47:08 +0100

    Added additional tests for stringbuilder variant of IsReserved

--- src/MooVC.Syntax.CSharp.Tests/Constructs/KeywordsTests/WhenIsReservedIsCalled.cs
+++ src/MooVC.Syntax.CSharp.Tests/Constructs/KeywordsTests/WhenIsReservedIsCalled.cs
@@ -1,50 +1,52 @@
 ﻿namespace MooVC.Syntax.CSharp.Constructs.KeywordsTests;
 
+using System.Text;
+
 public sealed class WhenIsReservedIsCalled
 {
     [Theory]
     [InlineData(null)]
     [InlineData("")]
     [InlineData("   ")]
-    public void GivenNullEmptyOrWhitespaceThenReturnsFalse(string? value)
+    public void GivenNullEmptyOrWhitespaceWhenStringThenReturnsFalse(string? value)
     {
         // Arrange & Act
-        bool result = Keywords.IsReserved(value);
+        bool result = value.IsReserved();
 
         // Assert
         result.ShouldBeFalse();
     }
 
     [Fact]
-    public void GivenAReservedKeywordThenReturnsTrue()
+    public void GivenAReservedKeywordWhenStringThenReturnsTrue()
     {
         // Arrange
         int element = Random.Shared.Next(Keywords.Reserved.Count);
         string keyword = Keywords.Reserved.ElementAt(element);
 
         // Act
-        bool result = Keywords.IsReserved(keyword);
+        bool result = keyword.IsReserved();
 
         // Assert
         result.ShouldBeTrue();
     }
 
     [Fact]
-    public void GivenAtPrefixedReservedKeywordThenReturnsFalse()
+    public void GivenAtPrefixedReservedKeywordWhenStringThenReturnsFalse()
     {
         // Arrange
         int element = Random.Shared.Next(Keywords.Reserved.Count);
         string keyword = Keywords.Reserved.ElementAt(element);
 
         // Act
-        bool result = Keywords.IsReserved($"@{keyword}");
+        bool result = $"@{keyword}".IsReserved();
 
         // Assert
         result.ShouldBeFalse();
     }
 
     [Fact]
-    public void GivenPascalCaseReservedKeywordThenReturnsFalse()
+    public void GivenPascalCaseReservedKeywordWhenStringThenReturnsFalse()
     {
         // Arrange
         int element = Random.Shared.Next(Keywords.Reserved.Count);
@@ -54,7 +56,7 @@ public sealed class WhenIsReservedIsCalled
             .ToPascalCase();
 
         // Act
-        bool result = Keywords.IsReserved(keyword);
+        bool result = keyword.IsReserved();
 
         // Assert
         result.ShouldBeFalse();
@@ -67,10 +69,106 @@ public sealed class WhenIsReservedIsCalled
     [InlineData("class\t")]
     [InlineData("\nclass")]
     [InlineData("class\n")]
-    public void GivenLeadingOrTrailingWhitespaceThenReturnsFalse(string value)
+    public void GivenLeadingOrTrailingWhitespaceWhenStringThenReturnsFalse(string value)
     {
         // Arrange & Act
-        bool result = Keywords.IsReserved(value);
+        bool result = value.IsReserved();
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void GivenNullWhenStringBuilderThenReturnsFalse()
+    {
+        // Arrange
+        StringBuilder? builder = default;
+
+        // Act
+        bool result = builder.IsReserved();
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void GivenEmptyOrWhitespaceWhenStringBuilderThenReturnsFalse(string value)
+    {
+        // Arrange
+        var builder = new StringBuilder(value);
+
+        // Act
+        bool result = builder.IsReserved();
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void GivenAReservedKeywordWhenStringBuilderThenReturnsTrue()
+    {
+        // Arrange
+        int element = Random.Shared.Next(Keywords.Reserved.Count);
+        string keyword = Keywords.Reserved.ElementAt(element);
+        var builder = new StringBuilder(keyword);
+
+        // Act
+        bool result = builder.IsReserved();
+
+        // Assert
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void GivenAtPrefixedReservedKeywordWhenStringBuilderThenReturnsFalse()
+    {
+        // Arrange
+        int element = Random.Shared.Next(Keywords.Reserved.Count);
+        string keyword = Keywords.Reserved.ElementAt(element);
+        var builder = new StringBuilder($"@{keyword}");
+
+        // Act
+        bool result = builder.IsReserved();
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void GivenPascalCaseReservedKeywordWhenStringBuilderThenReturnsFalse()
+    {
+        // Arrange
+        int element = Random.Shared.Next(Keywords.Reserved.Count);
+
+        string keyword = Keywords.Reserved
+            .ElementAt(element)
+            .ToPascalCase();
+
+        var builder = new StringBuilder(keyword);
+
+        // Act
+        bool result = builder.IsReserved();
+
+        // Assert
+        result.ShouldBeFalse();
+    }
+
+    [Theory]
+    [InlineData(" class")]
+    [InlineData("class ")]
+    [InlineData("\tclass")]
+    [InlineData("class\t")]
+    [InlineData("\nclass")]
+    [InlineData("class\n")]
+    public void GivenLeadingOrTrailingWhitespaceWhenStringBuilderThenReturnsFalse(string value)
+    {
+        // Arrange
+        var builder = new StringBuilder(value);
+
+        // Act
+        bool result = builder.IsReserved();
 
         // Assert
         result.ShouldBeFalse();
