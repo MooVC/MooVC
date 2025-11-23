818290a674d8deb10e69567984b60acb06117669
Paul Martins <50200071+MooVC@users.noreply.github.com>
2025-11-23 19:22:33 +0000

    Add declaration Fluentify extension tests

---
+++ src/MooVC.Syntax.CSharp.Tests/Members/DeclarationTests/WhenWithParametersIsCalled.cs
@@ -0,0 +1,23 @@
+namespace MooVC.Syntax.CSharp.Members.DeclarationTests;
+
+using MooVC.Syntax.CSharp.Generics;
+
+public sealed class WhenWithParametersIsCalled
+{
+    [Fact]
+    public void GivenParametersThenReturnsNewInstanceWithUpdatedParameters()
+    {
+        // Arrange
+        Declaration original = DeclarationTestsData.Create(parameterNames: "T");
+        Parameter[] additional = [new Parameter { Name = new Identifier("U") }];
+
+        // Act
+        Declaration result = original.WithParameters(additional);
+
+        // Assert
+        result.ShouldNotBeSameAs(original);
+        result.Parameters.Length.ShouldBe(2);
+        result.Parameters.ShouldBe(original.Parameters.Concat(additional));
+        result.Name.ShouldBe(original.Name);
+    }
+}
