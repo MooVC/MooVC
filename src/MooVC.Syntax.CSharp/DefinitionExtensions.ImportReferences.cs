2d5f76e0c5f5ef98b16cd77351e67abee15c49a9
Paul Martins <vmartins.paul@gmail.com>
2026-04-11 20:04:26 +0100

    Added exclusion ability to import references

--- src/MooVC.Syntax.CSharp/DefinitionExtensions.ImportReferences.cs
+++ src/MooVC.Syntax.CSharp/DefinitionExtensions.ImportReferences.cs
@@ -1,5 +1,7 @@
 ﻿namespace MooVC.Syntax.CSharp
 {
+    using System;
+    using System.Linq;
     using Ardalis.GuardClauses;
     using static MooVC.Syntax.CSharp.DefinitionExtensions_Resources;
 
@@ -12,12 +14,35 @@
         /// Imports all referenced qualifiers from the definition's type into the definition reference list.
         /// </summary>
         /// <param name="definition">The definition to update. Cannot be <see langword="null" />.</param>
+        /// <param name="exclusions">The qualifiers to exclude. Cannot be <see langword="null" />.</param>
         /// <returns>The updated definition.</returns>
-        public static Definition ImportReferences(this Definition definition)
+        public static Definition ImportReferences(this Definition definition, params Qualifier[] exclusions)
         {
             _ = Guard.Against.Null(definition, message: ImportReferencesDefinitionRequired);
 
-            return definition.Enumerate((qualifier, current) => current.Referencing(qualifier), qualifiers => qualifiers);
+            Definition ApplyWithExclusions(Qualifier qualifier, Definition current)
+            {
+                if (exclusions.Any(exclusion => exclusion == qualifier))
+                {
+                    return current;
+                }
+
+                return ApplyWithoutExclusions(qualifier, current);
+            }
+
+            Definition ApplyWithoutExclusions(Qualifier qualifier, Definition current)
+            {
+                return current.Referencing(qualifier);
+            }
+
+            Func<Qualifier, Definition, Definition> action = ApplyWithExclusions;
+
+            if (exclusions is null || exclusions.Length == 0)
+            {
+                action = ApplyWithoutExclusions;
+            }
+
+            return definition.Enumerate(action, qualifiers => qualifiers);
         }
     }
 }
